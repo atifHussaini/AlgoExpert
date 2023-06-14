5534ce43b74b1f4d8df180dfa1426b949eedbf54
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-14 13:41:38 -0500

    TwoNumberSum correct

--- Easy/TwoNumberSum.js
+++ Easy/TwoNumberSum.js
@@ -1,39 +1,22 @@
-// function twoNumberSum(array, targetSum) {
-//    let finalArr = [];
-//     let sortedArr = array.sort((a,b) => a-b)
-//     console.log(sortedArr)
-//     for (let i = 0; i < array.length; i++) {
-//       let elementOne = array[i]
-  
-//       for (let j = i+1; j < array.length; j++) {
-//         let elementTwo = array[j]
-  
-//         if ((elementOne + elementTwo) === targetSum) {
-//           finalArr.push(elementOne, elementTwo);
-//         }
-//       }
-//     }
-    
-//     return finalArr;
-//   }
-
-// console.log(twoNumberSum([3,5,-4,8,11,1,-1,6], 10))
-
 function twoNumberSum(array, targetSum) {
-    let nums = {};
-    
-    for (let num of array) {
-        let potentialMatch = targetSum - num;
-        if (potentialMatch in nums) {
-        return [potentialMatch, num];
-        } else {
-        nums[num] = true;
-        }
+    array.sort((a,b) => a - b);
+    let leftIdx = 0;
+    let rightIdx = array.length -1;
+  
+    while (leftIdx < rightIdx) {
+      const left = array[leftIdx];
+      const right = array[rightIdx];
+      
+      if (left + right === targetSum) {
+        return [left, right];
+      } else if (left + right < targetSum) {
+        leftIdx++;
+      } else {
+        rightIdx--
+      }
     }
-    
+  
     return [];
-}
-    
-
+  }
 
 console.log(twoNumberSum([3,5,-4,8,11,1,-1,6], 10))
