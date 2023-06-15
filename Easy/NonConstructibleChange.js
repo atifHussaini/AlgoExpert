fe3ad8b23c80093a31185acc874c7759c032507f
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-14 20:17:56 -0500

    Non constructible coin complete

---
+++ Easy/NonConstructibleChange.js
@@ -0,0 +1,14 @@
+function nonConstructibleChange(coins) {
+    coins.sort((a,b) => a-b);
+    let sum = 0;
+  
+    for (let i = 0; i < coins.length; i++) {
+      const coin = coins[i];
+      
+      if (coin > sum + 1) return sum + 1;
+      sum += coin;
+    }
+    return sum + 1;
+  }
+
+  console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
