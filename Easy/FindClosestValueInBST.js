43ecfd9b2dc0d46eda9cbf26577dcba942eb3521
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-15 12:48:59 -0500

    Find Closest Value in BST complete

--- Easy/FindClosestValueInBST.js
+++ Easy/FindClosestValueInBST.js
@@ -1,49 +1,39 @@
-class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-}
+const tree = {
+    "nodes": [
+      {"id": "10", "left": "5", "right": "15", "value": 10},
+      {"id": "15", "left": "13", "right": "22", "value": 15},
+      {"id": "22", "left": null, "right": null, "value": 22},
+      {"id": "13", "left": null, "right": "14", "value": 13},
+      {"id": "14", "left": null, "right": null, "value": 14},
+      {"id": "5", "left": "2", "right": "5-2", "value": 5},
+      {"id": "5-2", "left": null, "right": null, "value": 5},
+      {"id": "2", "left": "1", "right": null, "value": 2},
+      {"id": "1", "left": null, "right": null, "value": 1}
+    ],
+    "root": "10"
+  }
 
-const tree = new BST ( "nodes", 
-     [
-        {"id": "10", "left": "5", "right": "15", "value": 10},
-        {"id": "15", "left": "13", "right": "22", "value": 15},
-        {"id": "22", "left": null, "right": null, "value": 22},
-        {"id": "13", "left": null, "right": "14", "value": 13},
-        {"id": "14", "left": null, "right": null, "value": 14},
-        {"id": "5", "left": "2", "right": "5-2", "value": 5},
-        {"id": "5-2", "left": null, "right": null, "value": 5},
-        {"id": "2", "left": "1", "right": null, "value": 2},
-        {"id": "1", "left": null, "right": null, "value": 1}
-      ]
-  )
+const target = 12;
 
 function findClosestValueInBst(tree, target) {
-  let current = tree;
-  let bestNode = null;
-  let bestDiff = Infinity;
-  let stack = [[current, bestNode, bestDiff]];
+    const workingArr = [tree];
+    let minDiff = Infinity;
+    let closestVal;
   
-  while (stack.length) {
-    [current, bestNode, bestDiff] = stack.pop();
-    const currentDiff = Math.abs(target - current.value);
-
-    if (currentDiff === 0) {
-      return current.value;
+    while (workingArr.length) {
+      const node = workingArr.shift();
+      const currentDiff = Math.abs(target - node.value)
+  
+      if (currentDiff < minDiff) {
+        minDiff = currentDiff;
+        closestVal = node.value;
+      }
+  
+      if (node.value === target) return node.value;
+      if (node.value < target && node.right !== null) workingArr.push(node.right);
+      if (node.value > target && node.left !== null) workingArr.push(node.left);
     }
-
-    if (currentDiff < bestDiff) {
-      bestNode = current.value;
-      bestDiff = currentDiff;
-    }
-
-    if (current.right) stack.push([current.right, bestNode, bestDiff]);
-    if (current.left) stack.push([current.left, bestNode, bestDiff]);
-
+    return closestVal;
   }
-  console.log(bestNode)
-  return bestNode;
-}
-console.log(findClosestValueInBst(tree, 12))
+
+console.log(findClosestValueInBst(tree, target))
