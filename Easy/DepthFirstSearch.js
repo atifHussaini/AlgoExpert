f20da411abdc82ec00522762ac4e60867f60b342
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-15 15:58:50 -0500

    Depth first search done but need understanding

---
+++ Easy/DepthFirstSearch.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  
+    addChild(name) {
+      this.children.push(new Node(name));
+      return this;
+    }
+  
+    depthFirstSearch(array) {
+      array.push(this.name);
+      
+      if (this.children) {
+        for (let i = 0; i < this.children.length; i++) {
+          this.children[i].depthFirstSearch(array);
+        }
+      }
+      return array;
+    }
+}
