0c67969ddf858e2458baf65fd676ce8c60364280
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-06-18 16:10:13 -0500

    Binary Search complete

---
+++ Easy/BinarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(array, target) {
+    let leftPointer = 0;
+    let rightPointer = array.length-1;
+    
+    while (leftPointer <= rightPointer) {
+      const middleIndex = Math.floor((leftPointer + rightPointer) / 2);
+      const compare = array[middleIndex];
+  
+      if (target === compare) {
+        return middleIndex;
+      } else if (target < compare) {
+        rightPointer = middleIndex - 1;
+      } else {
+        leftPointer = middleIndex + 1;
+      }
+    }
+    return -1;
+  }
+
+console.log(binarySearch([0,1,21,32,45,45,61,71,72,73], 0))
--- Easy/PalindromeCheck.js
+++
@@ -1,9 +0,0 @@
-let list = [4,5,6,7,8]
-
-for (const key in list) {
-    console.log(key)
-}
-
-for (const key of list) {
-    console.log(key)
-}
