ef42b1f8ec32e7a0453fc3827f479662497e0485
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-07-03 13:23:31 -0500

    Bubble sort finished

--- Easy/BubbleSort.js
+++ Easy/BubbleSort.js
@@ -1,24 +1,23 @@
 function bubbleSort(array) {
-    for (let i = 0; i < array.length; i++) {
-      let isSwapped = false;
-      
-      for (let j = 0; j < array.length; j++) {
-        
-        if(array[j] > array[j+1]) {
-          let temp = array[j];
-          array[j] = array[j+1];
-          array[j+1] = temp;
-          isSwapped = true;
-        }
-        
-      }
-      
-      if(!isSwapped) {
-          break;
-      }
-    }
+  let count = 0;
+  let isSorted = false;
+
+  while (!isSorted) {
+    isSorted = true;
     
-    return array;
+    for (let i = 0; i < array.length - 1 - count; i++) {
+      const current = array[i];
+      const compare = array[i+1];
+
+      if (current > compare) {
+        array[i] = compare;
+        array[i+1] = current;
+        isSorted = false;
+      } 
+    }
+    count++;
   }
+  return array;
+}
 
   console.log(bubbleSort([8, 5, 2, 9, 5, 6, 3]))
