87e6bff1762a96d141b4004f64df500490f97104
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-08-15 16:19:30 -0500

    canContruct being optimized

--- Practice/DynamicProgramming/CanConstruct.js
+++ Practice/DynamicProgramming/CanConstruct.js
@@ -13,4 +13,4 @@ function canConstruct (target, wordBank) {
 
 console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))
 console.log(canConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))
-console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
+// console.log(canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))
