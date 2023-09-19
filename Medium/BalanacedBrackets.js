2fa92fc3ec92a31e0ddd85fcc073d5fa7c881704
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-09-19 14:06:54 -0500

    Balanced brackets complete

--- Medium/BalanacedBrackets.js
+++ Medium/BalanacedBrackets.js
@@ -1,54 +1,89 @@
+// function balancedBrackets(string) {
+//     const stringArr = string.split("");
+
+//   for (let i = 0; i < stringArr.length; i++) {
+//     const current = stringArr[i];
+
+//     if (current === '[') {
+//       let complete = false;
+      
+//       for (let j = i+1; j < stringArr.length; j++) {
+//         const compare = stringArr[j];
+
+//         if (compare === ']') {
+//           stringArr.splice(j,1);
+//           complete = true;
+//           break;
+//         }
+//       }
+//       if (!complete) return false; 
+//     }
+//     if (current === '(') {
+//       let complete = false;
+      
+//       for (let j = i+1; j < stringArr.length; j++) {
+//         const compare = stringArr[j];
+
+//         if (compare === ')') {
+//           stringArr.splice(j,1);
+//           complete = true;
+//           break;
+//         }
+//       }
+//       if (!complete) return false;
+//     }
+//     if (current === '{') {
+//       let complete = false;
+      
+//       for (let j = i+1; j < stringArr.length; j++) {
+//         const compare = stringArr[j];
+
+//         if (compare === '}') {
+//           stringArr.splice(j,1);
+//           complete = true;
+//           break;
+//         }
+//       }
+//       if (!complete) return false;
+//     }
+//     if (current === ']' || current === ')' || current === '}') return false;
+//   }
+//   return true;
+// }
+
 function balancedBrackets(string) {
-    const stringArr = string.split("");
+  const stack = [];
 
-  for (let i = 0; i < stringArr.length; i++) {
-    const current = stringArr[i];
+  for (let i = 0; i < string.length; i++) {
+    const current = string[i];
+    const peak = stack[stack.length-1];
 
-    if (current === '[') {
-      let complete = false;
-      
-      for (let j = i+1; j < stringArr.length; j++) {
-        const compare = stringArr[j];
-
-        if (compare === ']') {
-          stringArr.splice(j,1);
-          complete = true;
-          break;
+    if (current === "[" || current === "{" || current === "(") stack.push(current);
+    else {
+      if (current === "]") {
+        if (peak === "[") {
+          stack.pop();
+        } else {
+          return false;
         }
-      }
-      if (!complete) return false; 
-    }
-    if (current === '(') {
-      let complete = false;
-      
-      for (let j = i+1; j < stringArr.length; j++) {
-        const compare = stringArr[j];
-
-        if (compare === ')') {
-          stringArr.splice(j,1);
-          complete = true;
-          break;
+      } else if (current === "}") {
+        if (peak === "{") {
+          stack.pop();
+        } else {
+          return false;
         }
-      }
-      if (!complete) return false;
-    }
-    if (current === '{') {
-      let complete = false;
-      
-      for (let j = i+1; j < stringArr.length; j++) {
-        const compare = stringArr[j];
-
-        if (compare === '}') {
-          stringArr.splice(j,1);
-          complete = true;
-          break;
+      } else if (current === ")") {
+        if (peak === "(") {
+          stack.pop();
+        } else {
+          return false;
         }
+      } else {
+        return false;
       }
-      if (!complete) return false;
     }
-    if (current === ']' || current === ')' || current === '}') return false;
   }
-  return true;
+  return stack.length === 0;
 }
 
-  console.log(balancedBrackets("{[[[[({(}))]]]]}"))     
+console.log(balancedBrackets("(a)"))     
