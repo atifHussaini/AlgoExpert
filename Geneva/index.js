f20045ac898078bcaf263c1745a808de33caba20
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-10-02 16:25:33 -0500

    suffix stripping

--- Geneva/index.js
+++ Geneva/index.js
@@ -35,22 +35,22 @@ function lambdaMap (arr) {
 //             print(' '.join(map(str, row)))
 
 
-function get_dict_value(dct, path) {
-    path = path.replace(/\./g, "");
-    let final = null;
+// function get_dict_value(dct, path) {
+//     path = path.replace(/\./g, "");
+//     let final = null;
 
-    while (path.length) {
-        Object.entries(dct).forEach(([key, value]) => {
-            if (path.includes(key)) {
-                path = path.replace(key,"");
-                dct = value;
-                final = value;
-            }
-        }
-        );
-    }
-    return final;
-}   
+//     while (path.length) {
+//         Object.entries(dct).forEach(([key, value]) => {
+//             if (path.includes(key)) {
+//                 path = path.replace(key,"");
+//                 dct = value;
+//                 final = value;
+//             }
+//         }
+//         );
+//     }
+//     return final;
+// }   
 
 // console.log(get_dict_value({
 //     "student" : {
@@ -64,6 +64,38 @@ function get_dict_value(dct, path) {
 //         "school" : "ABC"
 //     }
 // }, "student.roll_number.friends"))
+
+//ed, ing, ly
+
+function suffixStripping (string) {
+    const workingArr = string.split(" ");
+    let finalStr = [];
+    
+    for (let i = 0; i < workingArr.length; i++) {
+        let word = workingArr[i];
+        const length = word.length;
+
+        if (word.endsWith("ing")) {
+            word = word.slice(0,length-3);
+        } else if (word.endsWith("ed")) {
+            word = word.slice(0, length-2);
+        } else if (word.endsWith("ly")) {
+            word = word.slice(0, length-2);
+        } 
+
+        if (length > 8) {
+           word = word.slice(0,8)
+        }
+        finalStr.push(word)
+    }
+
+    console.log(finalStr.join(" "));
+}
+
+// console.log(suffixStripping("an extremely dangerous dog is barking"))
+// console.log(suffixStripping("a boy is jumping quickly"))
+// console.log(suffixStripping("the results cannot be extrapolated to other patient group"))
+console.log(suffixStripping("the results cannoeding be extrapolated to other patient group"))
 
 
 
