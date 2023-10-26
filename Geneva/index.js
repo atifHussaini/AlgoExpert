7eb1cbe4355943c6f5d2f8c14048a81abc86b383
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-10-25 23:37:06 -0500

    add

--- Geneva/index.js
+++ Geneva/index.js
@@ -93,9 +93,9 @@ function suffixStripping (string) {
 }
 
 console.log(suffixStripping("an extremely dangerous dog is barking"))
-console.log(suffixStripping("a boy is jumping quickly"))
-console.log(suffixStripping("the results cannot be extrapolated to other patient group"))
-console.log(suffixStripping("the results cannoeding be extrapolated to other patient group"))
+// console.log(suffixStripping("a boy is jumping quickly"))
+// console.log(suffixStripping("the results cannot be extrapolated to other patient group"))
+// console.log(suffixStripping("the results cannoeding be extrapolated to other patient group"))
 
 
 
