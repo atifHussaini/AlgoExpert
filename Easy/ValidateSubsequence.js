2e5fb5eb68eb04b7dbf86159e6dd9fc3f06e36e5
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2023-12-01 16:06:03 -0600

    add

--- Easy/ValidateSubsequence.js
+++ Easy/ValidateSubsequence.js
@@ -1,19 +1,45 @@
-function isValidSubsequence(array, sequence) {
-  let result = '';
-  let finder = 0;
-  let current = 0;
+// function isValidSubsequence(array, sequence) {
+//   let result = '';
+//   let finder = 0;
+//   let current = 0;
 
-  while (finder <= array.length) {
-    if (sequence[current] === array[finder]) {
-      result = true;
-      finder++;
-      current++;
-    } else {
-      result = false;
-      finder++
+//   while (finder <= array.length) {
+//     if (sequence[current] === array[finder]) {
+//       result = true;
+//       finder++;
+//       current++;
+//     } else {
+//       result = false;
+//       finder++
+//     }
+//   }
+//   return result;
+// }
+
+ 
+function isValidSubsequence(array, sequence) {
+  const seqLength = sequence.length;
+  let count = 0;
+  let start = 0;
+  let valid = false;
+  
+  while (count < seqLength && start !== array.length) {
+    for (let i = start; i < array.length;) {
+      const current = array[i];
+
+      if (current === sequence[count]) {
+        start++;
+        count++;
+        valid = true;
+        break;
+      } else {
+        start++;
+        valid = false;
+        break;
+      }
     }
   }
-  return result;
+  return valid;
 }
 
-  console.log(isValidSubsequence([5,1,22,25,6,-1,8,6,10], [1,-1,6,10]))
+// console.log(isValidSubsequence([1, 1, 6, 1], [1, 1, 1, 6]))
