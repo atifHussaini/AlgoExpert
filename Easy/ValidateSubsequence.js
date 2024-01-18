192f3b7ca8e323a73dbe7cab5f78aa5d576455f5
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-01-18 09:42:04 -0600

    validate subsequence

--- Easy/ValidateSubsequence.js
+++ Easy/ValidateSubsequence.js
@@ -17,29 +17,53 @@
 // }
 
  
-function isValidSubsequence(array, sequence) {
-  const seqLength = sequence.length;
-  let count = 0;
-  let start = 0;
-  let valid = false;
+// function isValidSubsequence(array, sequence) {
+//   const seqLength = sequence.length;
+//   let count = 0;
+//   let start = 0;
+//   let valid = false;
   
-  while (count < seqLength && start !== array.length) {
-    for (let i = start; i < array.length;) {
-      const current = array[i];
+//   while (count < seqLength && start !== array.length) {
+//     for (let i = start; i < array.length;) {
+//       const current = array[i];
 
-      if (current === sequence[count]) {
-        start++;
-        count++;
-        valid = true;
-        break;
-      } else {
-        start++;
-        valid = false;
+//       if (current === sequence[count]) {
+//         start++;
+//         count++;
+//         valid = true;
+//         break;
+//       } else {
+//         start++;
+//         valid = false;
+//         break;
+//       }
+//     }
+//   }
+//   return valid;
+// }
+
+function isValidSubsequence(array, sequence) {
+  const length = sequence.length;
+  let index = 0;
+  let finder = 0;
+  
+  while (index < length) {
+    const current = sequence[index];
+    let match = false;
+
+    for (let i = finder; i < array.length; i++) {
+      const compare = array[i];
+      
+      if (compare === current) {
+        match = true;
         break;
       }
     }
+
+    if (match === false) return false;
+    index++;
   }
-  return valid;
+  return true;
 }
 
-// console.log(isValidSubsequence([1, 1, 6, 1], [1, 1, 1, 6]))
+console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, -1, 10]));
