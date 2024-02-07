2045cc8d9f90edf2a2c55b18c57b75dabcd70c69
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-02-07 12:13:29 -0600

    non constructible sum

--- Easy/NonConstructibleChange.js
+++ Easy/NonConstructibleChange.js
@@ -1,14 +1,31 @@
-function nonConstructibleChange(coins) {
-    coins.sort((a,b) => a-b);
-    let sum = 0;
+// function nonConstructibleChange(coins) {
+//     coins.sort((a,b) => a-b);
+//     let sum = 0;
   
-    for (let i = 0; i < coins.length; i++) {
-      const coin = coins[i];
+//     for (let i = 0; i < coins.length; i++) {
+//       const coin = coins[i];
       
-      if (coin > sum + 1) return sum + 1;
-      sum += coin;
-    }
-    return sum + 1;
-  }
+//       if (coin > sum + 1) return sum + 1;
+//       sum += coin;
+//     }
+//     return sum + 1;
+//   }
 
-  console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
+function nonConstructibleChange(coins) {
+  coins.sort((a,b) => a-b);
+  let sum = 0;
+  
+  for (let i = 0; i < coins.length; i++) {
+    const current = coins[i];
+
+    if (sum + 1 < current) {
+      return sum + 1;
+    } else {
+      sum += current;
+    }
+  }
+  return sum + 1;
+}
+
+
+console.log(nonConstructibleChange([5, 7, 1, 1, 2, 3, 22]));
