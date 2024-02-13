5b84574affb6b27180dadd388455f86856806466
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-02-13 15:37:19 -0600

    add

--- Easy/ClassPhotos.js
+++ Easy/ClassPhotos.js
@@ -1,21 +1,47 @@
-function classPhotos(redShirtHeights, blueShirtHeights) {
-    redShirtHeights.sort((a,b) => a-b);
-    blueShirtHeights.sort((a,b) => a-b);
+// function classPhotos(redShirtHeights, blueShirtHeights) {
+//     redShirtHeights.sort((a,b) => a-b);
+//     blueShirtHeights.sort((a,b) => a-b);
   
-    if (redShirtHeights[0] > blueShirtHeights[0]) {
-      for (let i = 0; i < redShirtHeights.length; i++) {
-        if (redShirtHeights[i] <= blueShirtHeights[i]) {
-          return false;
-        }
-      }
-    } else {
-      for (let i = 0; i < blueShirtHeights.length; i++) {
-        if (redShirtHeights[i] >= blueShirtHeights[i]) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
+//     if (redShirtHeights[0] > blueShirtHeights[0]) {
+//       for (let i = 0; i < redShirtHeights.length; i++) {
+//         if (redShirtHeights[i] <= blueShirtHeights[i]) {
+//           return false;
+//         }
+//       }
+//     } else {
+//       for (let i = 0; i < blueShirtHeights.length; i++) {
+//         if (redShirtHeights[i] >= blueShirtHeights[i]) {
+//           return false;
+//         }
+//       }
+//     }
+//     return true;
+//   }
 
-  console.log(classPhotos([5, 8, 1, 3, 4],[6, 9, 2, 4, 5]))
+function classPhotos(redShirtHeights, blueShirtHeights) {
+  redShirtHeights.sort((a,b) => a-b);
+  blueShirtHeights.sort((a,b) => a-b);
+  const redStart = redShirtHeights[0];
+  const blueStart = blueShirtHeights[0];
+  
+  if (redStart > blueStart) {
+    for (let i = 0; i < redShirtHeights; i++) {
+      const redPerson = redShirtHeights[i];
+      const bluePerson = blueShirtHeights[i];
+
+      if (redPerson < bluePerson) return false;
+    }
+  } else if (redStart < blueStart) {
+      for (let i = 0; i < redShirtHeights; i++) {
+        const redPerson = redShirtHeights[i];
+        const bluePerson = blueShirtHeights[i];
+  
+        if (redPerson > bluePerson) return false;
+      }
+  } else {
+    return false;
+  }
+  return true;
+}
+
+console.log(classPhotos([19, 2, 4, 6, 2, 3, 1, 1, 4],[21, 5, 4, 4, 4, 4, 4, 4, 4]))
