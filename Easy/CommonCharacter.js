09b52aa9d25d36f10127c43d2f10235fd9830de9
Atif Hussaini <114316821+AtifHussaini@users.noreply.github.com>
2024-02-18 16:05:12 -0600

    add

--- Easy/CommonCharacter.js
+++ Easy/CommonCharacter.js
@@ -1,25 +1,54 @@
+// function commonCharacters(strings) {
+//     const completeMap = new Map();
+//     const commonArr = [];
+  
+//     for (let i = 0; i < strings.length; i++) {
+//       const workingSet = new Set(strings[i]);
+  
+//       for (let character of workingSet) {
+//          if (!completeMap.has(character)) {
+//           completeMap.set(character, 1);
+//         } else {
+//           completeMap.set(character, completeMap.get(character) + 1);
+//         }
+//       }
+//     }
+  
+//     for (let [character, value] of completeMap) {
+//       if (value === strings.length) {
+//         commonArr.push(character);
+//       }
+//     }
+//     return commonArr;
+// }
+
+
 function commonCharacters(strings) {
-    const completeMap = new Map();
-    const commonArr = [];
-  
-    for (let i = 0; i < strings.length; i++) {
-      const workingSet = new Set(strings[i]);
-  
-      for (let character of workingSet) {
-         if (!completeMap.has(character)) {
-          completeMap.set(character, 1);
-        } else {
-          completeMap.set(character, completeMap.get(character) + 1);
-        }
+  const length = strings.length;
+  const stringMap = new Map();
+
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i].split("");
+    const removeDuplicates = new Set(string);
+
+    for (let j = 0; j < removeDuplicates.length; j++) {
+      const character = removeDuplicates[j];
+      
+      if (!stringMap.has(character)) {
+        stringMap.set(character, 1);
+      } else {
+        stringMap.set(character, stringMap.get(character)+1);
       }
     }
+  }
   
-    for (let [character, value] of completeMap) {
-      if (value === strings.length) {
-        commonArr.push(character);
-      }
+  const finalArr = [];
+  for (let [character, value] of stringMap) {
+    if (value === length) {
+      finalArr.push(character);
     }
-    return commonArr;
+  }
+  return finalArr;
 }
 
 console.log(commonCharacters(["abc", "bcd", "cbaccd"]));
